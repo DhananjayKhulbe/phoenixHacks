3b4d3599500f87dfd28b8f4542c515db4ac5062c
DhananjayKhulbe <dkhulbe115@gmail.com>
2024-05-25 08:57:28 -0700

    react setup

---
+++ main_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a basic starter template.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
